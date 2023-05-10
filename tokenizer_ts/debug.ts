e7514a0937dacea581fe7692b602edb0f48ec91f
Shengyu Fu <shengyfu@microsoft.com>
2023-05-10 15:56:58 -0700

    Address PR comments

--- tokenizer_ts/debug.ts
+++ tokenizer_ts/debug.ts
@@ -1,22 +1,29 @@
-import {
-    createByModelName
-  } from './src/tokenizerBuilder.js';
+import { createByModelName } from "./src/tokenizerBuilder.js";
 const IM_START = "<|im_start|>";
 const IM_END = "<|im_end|>";
 const specialTokens: ReadonlyMap<string, number> = new Map([
   [IM_START, 100264],
-  [IM_END, 100265],
+  [IM_END, 100265]
 ]);
 
-//const str = "This is a TopGame";
 const str = "<|im_start|>Hello World<|im_end|>";
-//const str = "!";
+
 let tokenizer = null;
 const createTokenizer = async () => {
-    tokenizer = await createByModelName("gpt-3.5-turbo", specialTokens);
-    //tokenizer = await createByModelName("gpt2");
-    //var out = tokenizer.encode(str, Array.from(specialTokens.keys()));
-    var out = tokenizer.encodeTrimSuffix(str, 5, Array.from(specialTokens.keys()));
-    console.log(out);
-}
+  tokenizer = await createByModelName("gpt-3.5-turbo", specialTokens);
+  var out1 = tokenizer.encode(str, Array.from(specialTokens.keys()));
+  console.log(out1);
+  var out2 = tokenizer.encodeTrimSuffix(
+    str,
+    3,
+    Array.from(specialTokens.keys())
+  );
+  console.log(out2);
+  var out3 = tokenizer.encodeTrimPrefix(
+    str,
+    3,
+    Array.from(specialTokens.keys())
+  );
+  console.log(out3);
+};
 createTokenizer();
--- tokenizer_ts/test/tikTokenizer.test.ts
+++ tokenizer_ts/test/tikTokenizer.test.ts
@@ -1,149 +1,214 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import * as assert from 'assert';
-import * as fs from 'fs';
-import { suite, beforeEach } from 'mocha';
-import {
-  createByModelName
-} from '../src/tokenizerBuilder';
-import { TikTokenizer } from '../src/tikTokenizer';
+import * as assert from "assert";
+import * as fs from "fs";
+import { suite, beforeEach } from "mocha";
+import { createByModelName } from "../src/tokenizerBuilder";
+import { TikTokenizer } from "../src/tikTokenizer";
 const IM_START = "<|im_start|>";
 const IM_END = "<|im_end|>";
 const specialTokens: ReadonlyMap<string, number> = new Map([
   [IM_START, 100264],
-  [IM_END, 100265],
+  [IM_END, 100265]
 ]);
 
-suite('TikTokenizer Test Suite', function () {
+suite("TikTokenizer Test Suite", function() {
   let tokenizer: TikTokenizer;
   beforeEach(async () => {
     tokenizer = await createByModelName("gpt-3.5-turbo", specialTokens);
   });
 
-  test('!', () => {
+  test("!", () => {
     const str = "!";
     let encoded = tokenizer.encode(str);
     assert.deepStrictEqual(encoded, [0]);
-    assert.strictEqual(tokenizer.decode(encoded), str)
+    assert.strictEqual(tokenizer.decode(encoded), str);
   });
 
-  test('empty string', () => {
+  test("empty string", () => {
     const str = "";
     let encoded = tokenizer.encode(str);
     assert.deepStrictEqual(encoded, []);
-    assert.strictEqual(tokenizer.decode(encoded), str)
+    assert.strictEqual(tokenizer.decode(encoded), str);
   });
 
-  test('hello world', () => {
+  test("hello world", () => {
     const str = "Hello World!";
     let encoded = tokenizer.encode(str);
     assert.deepStrictEqual(encoded, [9906, 4435, 0]);
-    assert.strictEqual(tokenizer.decode(encoded), str)
+    assert.strictEqual(tokenizer.decode(encoded), str);
   });
 
-  test('special tokens - 1', () => {
+  test("special tokens - 1", () => {
     const str = "<|im_start|>Hello World<|im_end|>";
     let encoded = tokenizer.encode(str, Array.from(specialTokens.keys()));
     assert.deepStrictEqual(encoded, [100264, 9906, 4435, 100265]);
-    assert.strictEqual(tokenizer.decode(encoded), str)
+    assert.strictEqual(tokenizer.decode(encoded), str);
   });
 
-  test('special tokens - 2', () => {
+  test("special tokens - 2", () => {
     const str = "<|im_start|>Hello<|im_end|> World";
     let encoded = tokenizer.encode(str, Array.from(specialTokens.keys()));
     assert.deepStrictEqual(encoded, [100264, 9906, 100265, 4435]);
-    assert.strictEqual(tokenizer.decode(encoded), str)
+    assert.strictEqual(tokenizer.decode(encoded), str);
   });
 
-  test('special tokens - 3', () => {
+  test("special tokens - 3", () => {
     const str = "<|im_start|>Hello ⭐ World<|im_end|>";
     let encoded = tokenizer.encode(str, Array.from(specialTokens.keys()));
     assert.deepStrictEqual(encoded, [100264, 9906, 2928, 99834, 4435, 100265]);
-    assert.strictEqual(tokenizer.decode(encoded), str)
+    assert.strictEqual(tokenizer.decode(encoded), str);
   });
 
-  test('encode trim suffix', () => {
+  test("encode trim suffix", () => {
     const str = "<|im_start|>Hello World<|im_end|>";
     const encodedStr = "<|im_start|>Hello World";
-    let encoded = tokenizer.encodeTrimSuffix(str, 4, Array.from(specialTokens.keys()));
+    let encoded = tokenizer.encodeTrimSuffix(
+      str,
+      4,
+      Array.from(specialTokens.keys())
+    );
     assert.deepStrictEqual(encoded.tokenIds, [100264, 9906, 4435, 100265]);
     assert.deepStrictEqual(encoded.text, str);
 
-    encoded = tokenizer.encodeTrimSuffix(str, 5, Array.from(specialTokens.keys()));
+    encoded = tokenizer.encodeTrimSuffix(
+      str,
+      5,
+      Array.from(specialTokens.keys())
+    );
     assert.deepStrictEqual(encoded.tokenIds, [100264, 9906, 4435, 100265]);
     assert.deepStrictEqual(encoded.text, str);
 
-    encoded = tokenizer.encodeTrimSuffix(str, 3, Array.from(specialTokens.keys()));
+    encoded = tokenizer.encodeTrimSuffix(
+      str,
+      3,
+      Array.from(specialTokens.keys())
+    );
     assert.deepStrictEqual(encoded.tokenIds, [100264, 9906, 4435]);
     assert.deepStrictEqual(encoded.text, encodedStr);
-
   });
 
-  test('encode trim suffix - 2', () => {
+  test("encode trim suffix - 2", () => {
     const str = "<|im_start|>Hello TempWorld<|im_end|>";
     const encodedStr = "<|im_start|>Hello";
-    let encoded = tokenizer.encodeTrimSuffix(str, 5, Array.from(specialTokens.keys()));
-    assert.deepStrictEqual(encoded.tokenIds, [100264, 9906, 20539, 10343, 100265]);
+    let encoded = tokenizer.encodeTrimSuffix(
+      str,
+      5,
+      Array.from(specialTokens.keys())
+    );
+    assert.deepStrictEqual(encoded.tokenIds, [
+      100264,
+      9906,
+      20539,
+      10343,
+      100265
+    ]);
     assert.deepStrictEqual(encoded.text, str);
 
-    encoded = tokenizer.encodeTrimSuffix(str, 6, Array.from(specialTokens.keys()));
-    assert.deepStrictEqual(encoded.tokenIds, [100264, 9906, 20539, 10343, 100265]);
+    encoded = tokenizer.encodeTrimSuffix(
+      str,
+      6,
+      Array.from(specialTokens.keys())
+    );
+    assert.deepStrictEqual(encoded.tokenIds, [
+      100264,
+      9906,
+      20539,
+      10343,
+      100265
+    ]);
     assert.deepStrictEqual(encoded.text, str);
 
-    encoded = tokenizer.encodeTrimSuffix(str, 3, Array.from(specialTokens.keys()));
+    encoded = tokenizer.encodeTrimSuffix(
+      str,
+      3,
+      Array.from(specialTokens.keys())
+    );
     assert.deepStrictEqual(encoded.tokenIds, [100264, 9906]);
     assert.deepStrictEqual(encoded.text, encodedStr);
-
   });
 
-  test('encode trim prefix', () => {
+  test("encode trim prefix", () => {
     const str = "<|im_start|>Hello World<|im_end|>";
     const encodedStr = "Hello World<|im_end|>";
-    let encoded = tokenizer.encodeTrimPrefix(str, 4, Array.from(specialTokens.keys()));
+    let encoded = tokenizer.encodeTrimPrefix(
+      str,
+      4,
+      Array.from(specialTokens.keys())
+    );
     assert.deepStrictEqual(encoded.tokenIds, [100264, 9906, 4435, 100265]);
     assert.deepStrictEqual(encoded.text, str);
 
-    encoded = tokenizer.encodeTrimPrefix(str, 5, Array.from(specialTokens.keys()));
+    encoded = tokenizer.encodeTrimPrefix(
+      str,
+      5,
+      Array.from(specialTokens.keys())
+    );
     assert.deepStrictEqual(encoded.tokenIds, [100264, 9906, 4435, 100265]);
     assert.deepStrictEqual(encoded.text, str);
 
-    encoded = tokenizer.encodeTrimPrefix(str, 3, Array.from(specialTokens.keys()));
+    encoded = tokenizer.encodeTrimPrefix(
+      str,
+      3,
+      Array.from(specialTokens.keys())
+    );
     assert.deepStrictEqual(encoded.tokenIds, [9906, 4435, 100265]);
     assert.deepStrictEqual(encoded.text, encodedStr);
-
   });
 
-  test('encode trim prefix - 2', () => {
+  test("encode trim prefix - 2", () => {
     const str = "<|im_start|>HelloTemp World<|im_end|>";
     const encodedStr = " World<|im_end|>";
-    let encoded = tokenizer.encodeTrimPrefix(str, 5, Array.from(specialTokens.keys()));
-    assert.deepStrictEqual(encoded.tokenIds, [100264, 9906, 12427, 4435, 100265]);
+    let encoded = tokenizer.encodeTrimPrefix(
+      str,
+      5,
+      Array.from(specialTokens.keys())
+    );
+    assert.deepStrictEqual(encoded.tokenIds, [
+      100264,
+      9906,
+      12427,
+      4435,
+      100265
+    ]);
     assert.deepStrictEqual(encoded.text, str);
 
-    encoded = tokenizer.encodeTrimPrefix(str, 6, Array.from(specialTokens.keys()));
-    assert.deepStrictEqual(encoded.tokenIds, [100264, 9906, 12427, 4435, 100265]);
+    encoded = tokenizer.encodeTrimPrefix(
+      str,
+      6,
+      Array.from(specialTokens.keys())
+    );
+    assert.deepStrictEqual(encoded.tokenIds, [
+      100264,
+      9906,
+      12427,
+      4435,
+      100265
+    ]);
     assert.deepStrictEqual(encoded.text, str);
 
-    encoded = tokenizer.encodeTrimPrefix(str, 3, Array.from(specialTokens.keys()));
+    encoded = tokenizer.encodeTrimPrefix(
+      str,
+      3,
+      Array.from(specialTokens.keys())
+    );
     assert.deepStrictEqual(encoded.tokenIds, [4435, 100265]);
     assert.deepStrictEqual(encoded.text, encodedStr);
-
   });
 
+  test("tokenize source code - gpt-3.5", done => {
+    const source = fs.readFileSync("test/testdata/lib.rs.txt", "utf8");
+    const filePath = "test/testdata/tokens.json";
 
-  test('tokenize source code - gpt-3.5', () => {
-    let source = fs.readFileSync('test/testdata/lib.rs.txt', 'utf8');
-    const filePath = 'test/testdata/tokens.json';
-
-    fs.readFile(filePath, 'utf8', (err, data) => {
+    fs.readFile(filePath, "utf8", (err, data) => {
       assert.strictEqual(err, null);
       const jsonArray = JSON.parse(data) as Array<number>;
       let encoded = tokenizer.encode(source, Array.from(specialTokens.keys()));
       assert.deepStrictEqual(encoded.length, 5584);
       assert.deepStrictEqual(encoded, jsonArray);
-      assert.strictEqual(tokenizer.decode(encoded), source)
+      assert.strictEqual(tokenizer.decode(encoded), source);
+      done();
     });
   });
-
 });
